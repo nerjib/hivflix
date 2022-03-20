3ad2fb3732d730bf408130aa20349946e084ce50
nerjib <kabirnajib0@gmail.com>
2022-03-20 11:40:20 +0100

    token

--- src/auth/authDattiSignin.js
+++ src/auth/authDattiSignin.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.use(CokokieParser());
 const db = require('../dbs/index');
 
+router.get('/', (req,res)=>{
+return res.status(200).send({status:'success'})
+})
 router.post('/', async (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(400).send({ message: 'Some values are missing' });
@@ -14,7 +17,7 @@ router.post('/', async (req, res) => {
   if (!Helper.isValidEmail(req.body.email)) {
     return res.status(401).send({ message: 'Please enter a valid email address' });
   }
-  const text = 'SELECT * FROM dattiusers WHERE email = $1';
+  const text = 'SELECT * FROM users WHERE email = $1';
   try {
     const { rows } = await db.query(text, [req.body.email]);
     if (!rows[0]) {
