a05ad32febcb692448268c768315120d02002599
nerjib <kabirnajib0@gmail.com>
2022-03-20 11:47:17 +0100

    token

--- src/auth/authDattiSignin.js
+++ src/auth/authDattiSignin.js
@@ -1,11 +1,5 @@
 const express = require('express');
 
-const CokokieParser = require('cookie-parser');
-const Helper = require('../helpers/helper');
-
-const router = express.Router();
-router.use(CokokieParser());
-const db = require('../dbs/index');
 
 router.get('/', (req,res)=>{
 return res.status(200).send({status:'success'})
