71d20daec4525da1b3023fd9b7a7d0e6481ed430
nerjib <kabirnajib0@gmail.com>
2022-03-24 11:53:16 +0100

    datti quiz

--- src/controllers/dattiquiz.js
+++ src/controllers/dattiquiz.js
@@ -19,14 +19,14 @@ router.get('/', async (req, res) => {
 });
 
 router.put('/', async (req, res) => {
-  const text = 'UPDATE dattiquiz set winner=$1, active=$2 WHERE id = $3 and winner is null and active=$4 returning *';
+  const text = 'UPDATE dattiquiz set winner=$1, active=$2, winnerid=$5, anstime=$6 WHERE id = $3 and winner is null and active=$4 returning *';
   // console.log(req.params.id);
   try {
-    const { rows } = await db.query(text, [req.body.winner, 0, req.body.quizid, 1]);
-  //  if (!rows[0]) {
-    //  return res.status(404).send({ message: 'User not found' });
-    //}
-    return res.status(200).send(rows);
+    const { rows } = await db.query(text, [req.body.winner, 0, req.body.quizid, 1, req.body.winnerid, moment(new Date())]);
+  if (rows.length<1) {
+      return res.status(404).send({ message: 'Quiz has been answered' });
+    }
+    return res.status(200).send({message:'Congratulation! you will be contacted for your prize'});
   } catch (error) {
     return res.status(400).send(error);
   }
