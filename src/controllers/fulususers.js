a1783874e46d12b06aaae977c52048fb07041034
nerjib <kabirnajib0@gmail.com>
2022-04-24 19:46:46 +0100

    sort

--- src/controllers/fulususers.js
+++ src/controllers/fulususers.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   let search = req.params.id;
-  const text = `select * from fulususers where phone_no like '${req.params.id}%' limit 1`;
+  const text = `select * from fulususers where phone_no like '%${req.params.id}' limit 1`;
   // console.log(req.params.id);
 
   try {
